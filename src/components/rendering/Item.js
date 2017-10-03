0d0dbf47fea669bdfb27cb0ed779c0570214715a
aretheregods <aretheregods@gmail.com>
2017-10-03 19:59:59 -0400

    Converted everything from createVNode to JSX, and did some other stuff

--- src/components/rendering/Item.js
+++ src/components/rendering/Item.js
@@ -11,14 +11,14 @@ export function Item(props) {
             <div className="detail-box">
                 {item.type === 'ask' || item.type === 'show' ?
                 <Link to={'/item/' + item.id} className="detail-child story-link"> {item.title} </Link> :
-                <a href={item.url} className="detail-child story-link"> {item.title} </a>
+                <Link to={item.url} className="detail-child story-link"> {item.title} </Link>
                 }
                 {item.user &&
                     <Link to={'/user/' + item.user} className="detail-child user-link">by <span className="user-id">{item.user}</span></Link>
                 }
                 <p className="detail-child detail-box-bottom">
                 {item.points || "0"}
-                <span className="score-star">{String.fromCharCode(9734)}&nbsp;</span>
+                <span className="score-star">{String.fromCharCode(9733)}&nbsp;</span>
                 {item.time_ago}&nbsp;
                 <Link to={'/item/'  + item.id} className="bottom-child item-comments"> {!item.comments_count ?
                     <span className="item-comments-span">Discuss</span> :
@@ -28,5 +28,5 @@ export function Item(props) {
             </div>
         </div>
     )
-    
+
 }
