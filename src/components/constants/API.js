d2a2ed338cdc4b2fe45a5b4bde0670737311d5f1
aretheregods <aretheregods@gmail.com>
2017-10-03 16:10:11 -0400

    "Productionized" the API locations

--- src/components/constants/API.js
+++ src/components/constants/API.js
@@ -1,18 +1,18 @@
 // Hacker News api locations
 // Use these in development
-export const  hn_api_pages = {
-    news: (parameter) => `https://hnpwa.com/api/v0/news.json?page=${parameter}`,
-    newest: (parameter) => `https://hnpwa.com/api/v0/newest.json?page=${parameter}`,
-    show: (parameter) => `https://hnpwa.com/api/v0/show.json?page=${parameter}`,
-    ask: (parameter) => `https://hnpwa.com/api/v0/ask.json?page=${parameter}`,
-    jobs: (parameter) => `https://hnpwa.com/api/v0/jobs.json?page=${parameter}`,
-    user: (parameter) => `https://hnpwa.com/api/v0/user/${parameter}.json`,
-    item: (parameter) => `https://hnpwa.com/api/v0/item/${parameter}.json`
-}
+// export const  hn_api_pages = {
+//     news: (parameter) => `https://hnpwa.com/api/v0/news.json?page=${parameter}`,
+//     newest: (parameter) => `https://hnpwa.com/api/v0/newest.json?page=${parameter}`,
+//     show: (parameter) => `https://hnpwa.com/api/v0/show.json?page=${parameter}`,
+//     ask: (parameter) => `https://hnpwa.com/api/v0/ask.json?page=${parameter}`,
+//     jobs: (parameter) => `https://hnpwa.com/api/v0/jobs.json?page=${parameter}`,
+//     user: (parameter) => `https://hnpwa.com/api/v0/user/${parameter}.json`,
+//     item: (parameter) => `https://hnpwa.com/api/v0/item/${parameter}.json`
+// }
 
 
 
-/*// Use these in production
+// Use these in production
 export const hn_api_pages = {
     news: (parameter) => `/api/news.json?page=${parameter}`,
     newest: (parameter) => `/api/newest.json?page=${parameter}`,
@@ -21,4 +21,4 @@ export const hn_api_pages = {
     jobs: (parameter) => `/api/jobs.json?page=${parameter}`,
     user: (parameter) => `/api/user/${parameter}.json`,
     item: (parameter) => `/api/item/${parameter}.json`
-}*/
+}
